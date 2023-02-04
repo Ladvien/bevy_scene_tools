25de0fb15e8b32f3d34c1866e21f5250196760a1
C. Thomas Brittain <thomasb@bitfocus.com>
2023-02-04 07:56:12 -0600

    Clean work environment.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,12 @@ pub struct SceneToolsPlugin;
 
 impl Plugin for SceneToolsPlugin {
     fn build(&self, app: &mut App) {
-        let app = app;
+        let app = app.add_system(add_scene_aabbs);
+    }
+}
+
+fn add_scene_aabbs(query: Query<&Handle<Scene>>) -> () {
+    for scene in query.iter() {
+        println!("{:?}", scene);
     }
 }
