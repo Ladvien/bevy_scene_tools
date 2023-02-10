93acb4df4b6a1b487a38efab0fe77851176928a1
C. Thomas Brittain <thomasb@bitfocus.com>
2023-02-10 06:54:27 -0600

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,151 @@
-use bevy::{prelude::*, render::primitives::Aabb};
+use std::collections::HashMap;
+
+use bevy::{math::Vec3A, prelude::*, render::primitives::Aabb};
 
 #[derive(Component, Debug, Default)]
 pub struct Marker;
 
+#[derive(Component, Debug, Default)]
+struct AabbParsed;
+
 pub struct SceneToolsPlugin;
+
+#[derive(Debug, Default)]
+pub struct SceneNode {
+    pub position: Vec3,
+    pub aabb: Option<Aabb>,
+    pub mesh_handle: Option<Handle<Mesh>>,
+}
+
+impl SceneNode {
+    fn from(position: Vec3) -> Self {
+        Self {
+            position,
+            aabb: None,
+            mesh_handle: None,
+        }
+    }
+}
 
 impl Plugin for SceneToolsPlugin {
     fn build(&self, app: &mut App) {
-        let app = app.add_system(add_scene_aabbs);
+        let app = app.add_system(add_scene_nodes);
     }
 }
 
-fn add_scene_aabbs(
+fn add_scene_nodes(
     mut commands: Commands,
-    scenes: Query<Entity, With<Handle<Scene>>>,
+    scenes: Query<Entity, (With<Handle<Scene>>, Without<AabbParsed>)>,
     children: Query<&Children>,
-    meshes: Query<(Entity, &Aabb), (With<Handle<Mesh>>, Without<Marker>)>,
+    existing_meshes: Query<(Entity, &Aabb), (With<Handle<Mesh>>, Without<Marker>)>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    transforms: Query<&Transform>,
 ) -> () {
     for scene in scenes.iter() {
-        get_all_meshes_from_children(&mut commands, scene, &children, &meshes)
+        let mut scene_nodes: HashMap<Entity, SceneNode> = HashMap::new();
+
+        if let Ok(transform) = transforms.get(scene) {
+            // TODO: Load all scene node items from query.
+            scene_nodes.insert(
+                scene,
+                SceneNode {
+                    position: transform.translation,
+                    aabb: None,
+                    mesh_handle: None,
+                },
+            );
+        }
+
+        get_all_meshes_from_children(
+            &mut commands,
+            scene,
+            &children,
+            &existing_meshes,
+            &mut scene_nodes,
+            &transforms,
+        );
+        println!("Scene AABBs: {:#?}", scene_nodes);
+
+        // # TODO: For testing, replace with a proper flag to ensure
+        // scene is completely loaded before parsing.
+        if !scene_nodes.is_empty() {
+            commands.entity(scene).insert(AabbParsed);
+            for scene_node in scene_nodes.into_values() {
+                let aabb = scene_node.aabb.unwrap_or(Aabb {
+                    center: Vec3A::ZERO,
+                    half_extents: Vec3A::ZERO,
+                });
+
+                commands
+                    .spawn(PbrBundle {
+                        mesh: meshes.add(Mesh::from(box_mesh_from_aabb(&aabb))),
+                        transform: Transform {
+                            translation: Vec3::from(aabb.center),
+                            ..Default::default()
+                        },
+                        material: materials.add(StandardMaterial {
+                            base_color: Color::Rgba {
+                                red: 1.0,
+                                green: 0.0,
+                                blue: 0.0,
+                                alpha: 0.5,
+                            },
+                            emissive: Color::Rgba {
+                                red: 1.0,
+                                green: 0.0,
+                                blue: 0.0,
+                                alpha: 0.5,
+                            },
+                            ..Default::default()
+                        }),
+                        ..Default::default()
+                    })
+                    .insert(Name::from("debug-cube"));
+            }
+        }
     }
 }
 
-fn get_all_meshes_from_children(
+fn get_all_meshes_from_children<'a>(
     commands: &mut Commands,
     entity: Entity,
     children: &Query<&Children>,
     meshes: &Query<(Entity, &Aabb), (With<Handle<Mesh>>, Without<Marker>)>,
-) {
+    mut scene_nodes: &'a mut HashMap<Entity, SceneNode>,
+    transforms: &Query<&Transform>,
+) -> &'a mut HashMap<Entity, SceneNode> {
     if let Ok(_children) = children.get(entity) {
-        // println!("Children: {:?}", _children);
         for child in _children {
-            // println!("Child ID: {:?}", child);
-            if let Ok((mesh, aabb)) = meshes.get(*child) {
-                println!("Mesh ID: {:?}, AABB: {:?}", mesh, aabb);
-                // commands.entity(mesh).insert(Marker);
+            if let Ok(transform) = transforms.get(*child) {
+                let mut node = SceneNode::from(transform.translation);
+                if let Ok((mesh, aabb)) = meshes.get(*child) {
+                    println!("{:#?}", aabb);
+                    node.aabb = Some(aabb.to_owned());
+                }
             }
+
             commands.entity(*child).insert(Marker);
-            get_all_meshes_from_children(commands, *child, &children, &meshes)
+            scene_nodes = get_all_meshes_from_children(
+                commands,
+                *child,
+                &children,
+                &meshes,
+                scene_nodes,
+                transforms,
+            );
         }
+    }
+    scene_nodes
+}
+
+fn box_mesh_from_aabb(aabb: &Aabb) -> shape::Box {
+    shape::Box {
+        min_x: aabb.center.x - aabb.half_extents.x,
+        max_x: aabb.center.x + aabb.half_extents.x,
+        min_y: aabb.center.y - aabb.half_extents.y,
+        max_y: aabb.center.y + aabb.half_extents.y,
+        min_z: aabb.center.z - aabb.half_extents.z,
+        max_z: aabb.center.z + aabb.half_extents.z,
     }
 }
