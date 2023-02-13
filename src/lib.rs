789bf357f25f57aeaa95ed9de6b641856e02ea5c
Ladvien <cthomasbrittain@hotmail.com>
2023-02-13 17:51:23 -0600

    Working

--- src/lib.rs
+++ src/lib.rs
@@ -13,29 +13,23 @@ struct AabbParsed;
 
 pub struct SceneToolsPlugin;
 
-#[derive(Debug, Default)]
-pub struct SceneNode {
-    pub global_position: Vec3,
-    pub aabb: Aabb,
-}
-
 impl Plugin for SceneToolsPlugin {
     fn build(&self, app: &mut App) {
-        let app = app.add_system_to_stage(CoreStage::Last, add_scene_nodes);
+        let app = app.add_system_to_stage(CoreStage::Last, add_scene_aabbs);
     }
 }
 
-fn add_scene_nodes(
+fn add_scene_aabbs(
     mut commands: Commands,
     scenes: Query<Entity, (With<Handle<Scene>>, Without<AabbParsed>, Without<Marker>)>,
     children: Query<&Children>,
     existing_meshes: Query<(Entity, &Aabb), (With<Handle<Mesh>>, Without<Marker>)>,
-    mut aabbs: ResMut<Assets<Mesh>>,
+    mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     global_transforms: Query<&GlobalTransform>,
 ) -> () {
     for scene in scenes.iter() {
-        let mut scene_nodes: HashMap<Entity, SceneNode> = HashMap::new();
+        let mut scene_aabbs: HashMap<Entity, Aabb> = HashMap::new();
         // println!("Parsing scene: {:?}", scene);
 
         let scene_center = match global_transforms.get(scene) {
@@ -46,98 +40,101 @@ fn add_scene_nodes(
         println!("scene_center: {:?}", scene_center);
 
         get_all_meshes_from_children(
+            scene_center,
             &mut commands,
             scene,
             &children,
             &existing_meshes,
-            &mut scene_nodes,
+            &mut scene_aabbs,
             &global_transforms,
         );
 
         // # TODO: For testing, replace with a proper flag to ensure
         // scene is completely loaded before parsing.
-        if !scene_nodes.is_empty() {
-            println!("Scene AABBs: {:#?}", scene_nodes);
-            //     commands.entity(scene).insert(AabbParsed);
-            //     for scene_node in scene_nodes.into_values() {
-            //         let aabb = scene_node.aabb.unwrap_or(Aabb {
-            //             center: Vec3A::ZERO,
-            //             half_extents: Vec3A::ZERO,
-            //         });
+        if !scene_aabbs.is_empty() {
+            println!("Scene AABBs: {:#?}", scene_aabbs);
+                commands.entity(scene).insert(AabbParsed);
+                for scene_aabb in scene_aabbs.into_values() {
 
-            //         commands
-            //             .spawn(PbrBundle {
-            //                 mesh: meshes.add(Mesh::from(box_mesh_from_aabb(&aabb))),
-            //                 transform: Transform {
-            //                     translation: Vec3::from(aabb.center),
-            //                     ..Default::default()
-            //                 },
-            //                 material: materials.add(StandardMaterial {
-            //                     base_color: Color::Rgba {
-            //                         red: 1.0,
-            //                         green: 0.0,
-            //                         blue: 0.0,
-            //                         alpha: 0.5,
-            //                     },
-            //                     emissive: Color::Rgba {
-            //                         red: 1.0,
-            //                         green: 0.0,
-            //                         blue: 0.0,
-            //                         alpha: 0.5,
-            //                     },
-            //                     ..Default::default()
-            //                 }),
-            //                 ..Default::default()
-            //             })
-            //             .insert(Name::from("debug-cube"));
-            //     }
+                    commands
+                        .spawn(PbrBundle {
+                            mesh: meshes.add(Mesh::from(box_mesh_from_aabb(&scene_aabb))),
+                            transform: Transform {
+                                translation: Vec3::from(scene_aabb.center),
+                                ..Default::default()
+                            },
+                            material: materials.add(StandardMaterial {
+                                base_color: Color::Rgba {
+                                    red: 1.0,
+                                    green: 0.0,
+                                    blue: 0.0,
+                                    alpha: 0.5,
+                                },
+                                emissive: Color::Rgba {
+                                    red: 1.0,
+                                    green: 0.0,
+                                    blue: 0.0,
+                                    alpha: 0.5,
+                                },
+                                ..Default::default()
+                            }),
+                            ..Default::default()
+                        })
+                        .insert(Name::from("debug-cube"));
+                }
             commands.entity(scene).insert(Marker);
         }
     }
 }
 
 fn get_all_meshes_from_children<'a>(
+    scene_center: Vec3,
     commands: &mut Commands,
     entity: Entity,
     children: &Query<&Children>,
     aabbs: &Query<(Entity, &Aabb), (With<Handle<Mesh>>, Without<Marker>)>,
-    mut scene_nodes: &'a mut HashMap<Entity, SceneNode>,
+    mut scene_aabbs: &'a mut HashMap<Entity, Aabb>,
     global_transforms: &Query<&GlobalTransform>,
-) -> &'a mut HashMap<Entity, SceneNode> {
+) -> &'a mut HashMap<Entity, Aabb> {
     if let Ok(_children) = children.get(entity) {
         for child in _children {
             // println!("Child {:?}", child);
             commands.entity(*child).insert(Marker);
 
-            match get_scene_node(*child, aabbs, global_transforms) {
-                Ok(scene_node) => scene_nodes.insert(*child, scene_node),
+            match get_scene_aabb(scene_center, *child, aabbs, global_transforms) {
+                Ok(scene_aabb) => scene_aabbs.insert(*child, scene_aabb),
                 Err(_) => None,
             };
 
-            scene_nodes = get_all_meshes_from_children(
+            scene_aabbs = get_all_meshes_from_children(
+                scene_center,
                 commands,
                 *child,
                 &children,
                 &aabbs,
-                scene_nodes,
+                scene_aabbs,
                 global_transforms,
             );
         }
     }
-    scene_nodes
+    scene_aabbs
 }
 
-fn get_scene_node(
+fn get_scene_aabb(
+    scene_center: Vec3,
     entity: Entity,
     aabbs: &Query<(Entity, &Aabb), (With<Handle<Mesh>>, Without<Marker>)>,
     global_transforms: &Query<&GlobalTransform>,
-) -> Result<SceneNode, QueryEntityError> {
+) -> Result<Aabb, QueryEntityError> {
     let global_transform = global_transforms.get(entity)?;
     let (entity, aabb) = aabbs.get(entity)?;
-    println!("Global transform: {:?}", global_transform);
-    Ok(SceneNode {
-        global_position: global_transform.translation(),
-        aabb: aabb.clone(),
+
+    let scene_half_extents = scene_center + (Vec3::from(aabb.center) + Vec3::from(aabb.half_extents));
+    println!("scene_center: {:?}", scene_center);
+    println!("global_transform: {:?}", global_transform.translation());
+    Ok(Aabb {
+        center: Vec3A::from(scene_center),
+        half_extents: Vec3A::from(scene_half_extents),
     })
 }
 
